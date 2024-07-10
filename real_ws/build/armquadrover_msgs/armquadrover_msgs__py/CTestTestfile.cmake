a4935876810854b3aec6f90d228a8be84cccb95d
Quillon Sun <s100330@126.com>
2024-07-10 15:34:51 +0800

    修正

--- real_ws/build/armquadrover_msgs/armquadrover_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/real_ws/build/armquadrover_msgs/armquadrover_msgs__py
-# Build directory: /root/real_ws/build/armquadrover_msgs/armquadrover_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- real_ws/build/armquadrover_msgs/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(armquadrover_msgs_IDL_FILES "msg/DDSM115.idl;msg/DDSM115Servo.idl")
-set(armquadrover_msgs_INTERFACE_FILES "msg/DDSM115.msg;msg/DDSM115Servo.msg")
